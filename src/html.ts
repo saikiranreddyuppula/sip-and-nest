ead8c61a84749a5d2519ec9a215841bcc42a5e60
Uppula Sai Kiran Reddy <32011704+saikiranreddyuppula@users.noreply.github.com>
2026-09-01 09:17:23 -0400

    Restore brown Reserve html.ts (intro home-only, scroll fix)

--- src/html.ts
+++ src/html.ts
@@ -1,17 +1,1 @@
-import { pickupDays, pickupSlots, site } from "./config";
-import {
-  formatCents,
-  parseSizes,
-  type CoffeeType,
-  type OrderLine,
-  type OrderRow,
-} from "./db";
-
-export function esc(s: string): string {
-  return s
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;")
-    .replace(/'/g, "&#39;");
-}
+FILE_FROM_DISK:/workspace/sip-and-nest/src/html.ts
