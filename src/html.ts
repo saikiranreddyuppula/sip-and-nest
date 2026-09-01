c435f5d26804abe6e7e956948bb9d6411b40a970
Uppula Sai Kiran Reddy <32011704+saikiranreddyuppula@users.noreply.github.com>
2026-08-31 20:22:24 -0400

    Restore dark brass Sip & Nest theme on main

--- src/html.ts
+++ src/html.ts
@@ -1,17 +1,1 @@
-import { pickupDays, pickupSlots, site } from "./config";
-import {
-  formatCents,
-  parseSizes,
-  type CoffeeType,
-  type OrderLine,
-  type OrderRow,
-} from "./db";
-
-export function esc(s: string): string {
-  return s
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;")
-    .replace(/'/g, "&#39;");
-}
+SEE_WORKSPACE_FILE_html_ts
